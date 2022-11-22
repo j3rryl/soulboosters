7a2c348f7e1c1c61cba8ea3e14231f99a9542633
j3rryl <jeremymunroe0@gmail.com>
2022-11-22 19:34:42 +0300

    faq changes

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,20 +1,7 @@
-import { useEffect, useState } from "react"
-
 import '../assets/scss/faq.scss'
 import Questions from "../components/Questions"
 
 const Faq = () => {
-  const faqs = document.querySelectorAll(".faq")
-  const [faq,setFaq]=useState(
-    false)
-  function toggleFaq(){
-      faqs.forEach(faq=>{
-        faq.addEventListener("click", ()=>{
-          faq.classList.toggle("active")
-        })
-      })
-      setFaq(!faq)
-  }
   return (
     <div className="faq-page">
       <div className="faq-card">
