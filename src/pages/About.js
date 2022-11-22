2450d8eda61a90ee9ee03dad519ffa41fc21caf8
j3rryl <jeremymunroe0@gmail.com>
2022-11-22 10:27:13 +0300

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import '../assets/scss/about.scss'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about-page'>
+      <div className='container our-story'>
+        <h5>Our Story.</h5>
+      </div>
+      <div className="light-themed">
+        <h5>How It Started</h5>
+        <p>     
+        Climate change: two small words that represent a radical transformation of our planet, billions of data points, millions of climate scientists and campaigners, and a limitless number of stories from around the world. 
+        </p>
+      </div>
+      <div className='container our-strategy'>
+        <h5>Our Strategy.</h5>
+      </div>
+      <div className="light-themed">
+        <h5>The Plan</h5>
+        <p>     
+        To preserve a livable climate, greenhouse-gas emissions must be reduced by half by 2030 and to net zero by 2050. Bold, fast, and wide-ranging action needs to be taken by governments and businesses. 
+        But the transition to a low-carbon world also requires the participation of citizens - especially in advanced economies.
+        </p>
+      </div>
+      <div className='container our-people'>
+        <h5>Our People.</h5>
+      </div>
+      <div className="light-themed">
+        <h5>The People</h5>
+        <p>     
+        The African peoples of Kenya is divided into three language groups: Bantu, Nilo-Saharan, and Afro-Asiatic. 
+        Bantu is by far the largest, and its speakers are mainly concentrated in the southern third of the country. The Kikuyu, Kamba, Meru, and Nyika peoples occupy the fertile Central Rift highlands, while the Luhya and Gusii inhabit the Lake Victoria basin.
+        </p>
+      </div>
+      <div className='container our-partners'>
+        <h5>Our Partners.</h5>
+      </div>
+
+    </div>
   )
 }
 
